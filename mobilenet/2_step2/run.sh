71c2d119c12524937db4ddbf7a5afc22f7191615
Zechun Liu <362092891@qq.com>
2021-11-11 09:55:19 -0500

    Update run.sh

--- mobilenet/2_step2/run.sh
+++ mobilenet/2_step2/run.sh
@@ -2,4 +2,7 @@ clear
 mkdir models
 cp ../1_step1/models/checkpoint.pth.tar ./models/checkpoint_ba.pth.tar
 mkdir log
-python3 train.py --data=/datasets/imagenet --batch_size=256 --learning_rate=5e-4 --epochs=256 --weight_decay=0 | tee -a log/training.txt
+# 128 epoch setting: larger learning rate, similar performance to 256 epoch
+python3 train.py --data=/datasets/imagenet --batch_size=256 --learning_rate=1.25e-3 --epochs=128 --weight_decay=0 | tee -a log/training.txt
+# 256 epoch setting: longer training, similar performance to 128 epoch
+# python3 train.py --data=/datasets/imagenet --batch_size=256 --learning_rate=5e-4 --epochs=256 --weight_decay=0 | tee -a log/training.txt
