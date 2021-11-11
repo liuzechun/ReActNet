8f50116895ebcfca4ee0c29190e0c5383d53532e
Zechun Liu <362092891@qq.com>
2021-11-11 10:01:14 -0500

    Update run.sh

--- mobilenet/1_step1/run.sh
+++ mobilenet/1_step1/run.sh
@@ -1,4 +1,5 @@
 clear
+mkdir log
 # 128 epoch setting: larger learning rate, similar performance to 256 epoch
 python3 train.py --data=/datasets/imagenet --batch_size=256 --learning_rate=1.25e-3 --epochs=128 --weight_decay=1e-5 | tee -a log/training.txt
 # 256 epoch setting: longer training, similar performance to 128 epoch
