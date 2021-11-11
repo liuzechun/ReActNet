2c74bfd11bb0c28fd3dd617e9ffc45381dbb19cd
Zechun Liu <362092891@qq.com>
2021-11-11 09:59:48 -0500

    Update run.sh

--- resnet/2_step2/run.sh
+++ resnet/2_step2/run.sh
@@ -6,5 +6,3 @@ mkdir log
 python3 train.py --data=/datasets/imagenet --batch_size=512 --learning_rate=2.5e-3 --epochs=128 --weight_decay=0 | tee -a log/training.txt
 # 256 epoch setting: longer training, similar performance to 128 epoch
 # python3 train.py --data=/datasets/imagenet --batch_size=512 --learning_rate=1e-3 --epochs=256 --weight_decay=0 | tee -a log/training.txt
-
-
